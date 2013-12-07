e0360939a2717b02e3da71931c0bb3a1fab11e78
Rory <Rory@Rory-PC>
2013-12-06 21:44:05 -0500

    initial commit

---
+++ src/main/java/Euler.java
@@ -0,0 +1,53 @@
+public class Euler {
+    
+    public static int sumMult(int n)
+    {
+        int res = 0;
+        int mult = 0;
+        while(n*mult<1000){
+            res+=n*mult;
+            mult++;
+        }
+        return res;
+    }
+    
+    //another way:
+    public static int findRepeat(int n, int m)
+    {
+        int x = n*m;
+        return sumMult(x);
+    }
+    
+    public static int sumDiv(int n)
+    {
+        int res = 0;
+        for(int i = 1; i<=n; i++)
+        {
+            if(n%i==0)
+                res+=i;
+        }
+        return res;
+    }
+    
+    public static int sumSumDiv(int n)
+    {
+        int res = 0;
+        for(int i = 1; i<=n; i++)
+        {
+            for(int k=1; k<=n; k++)
+                res+=sumDiv(i*k);
+        }
+        return res;
+    }
+    
+    
+    public static void main(String[] args)
+    {
+        //int s = sumMult(3) + sumMult(5) - sumMult(3*5);
+        //System.out.println(s);
+
+        int s1 = sumSumDiv(1000)%1000000000;
+        System.out.println(s1);
+    }
+
+}
