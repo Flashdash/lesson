fa25ca7547c21c07f808e6ef49bc765f1b7a74a8
Rory <Rory@Rory-PC>
2013-12-08 14:20:50 -0500

    Reformatted it again...

--- src/main/java/Problem_1_PE.java
+++ src/main/java/Problem_1_PE.java
@@ -1,23 +1,23 @@
 public class Problem_1_PE {
-	public static void main(String[] args){
-int a = 0;
-int b = 0;
-int c = 0;
-int i;
-{
+	public static void main(String[] args) {
+		int a = 0;
+		int b = 0;
+		int c = 0;
+		int i;
+		{
 
-for (i=3; i<1000; i+=3)
-	a+=i;
-	
-for (i=5; i<1000; i+=5)
-	b+=i;
+			for (i = 3; i < 1000; i += 3)
+				a += i;
 
-for (i=15; i<1000; i+=15)
-	c+=i;
+			for (i = 5; i < 1000; i += 5)
+				b += i;
 
-int Sum=a+b-c;
+			for (i = 15; i < 1000; i += 15)
+				c += i;
 
-System.out.println(Sum);
-}
-}
+			int Sum = a + b - c;
+
+			System.out.println(Sum);
+		}
+	}
 }
