54a8d6d475caa2856d616c10f924f91c16718901
Rory <Rory@Rory-PC>
2013-12-29 15:56:32 -0500

    FINISHED YESYESYESYESYES

--- src/test/java/Problem_2_Test.java
+++ src/test/java/Problem_2_Test.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class Problem_2_Test {
 	@Test
 	public void test() {
-		int sum = Problem_2_PE.sum(1, 1, 0);
-		assertThat(sum, equalTo(44));
+		int sum = Problem_2_PE.sum(1, 1, 0, 4000000);
+		assertThat(sum, equalTo(4613732));
 	}
 }
